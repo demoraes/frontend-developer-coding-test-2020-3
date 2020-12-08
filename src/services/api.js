48bfe75a7f5e8d650891c0145f841509382233f7
demoraes <gabrielmoraes_2013@outlook.com>
2020-12-08 18:38:43 -0400

    edit baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_KEY = "ntzIk5iasX7PLW5ybOi-2aaWyRTA6PTwWi4Da9VWUJ-3CL9w8r5aV1hdg94JGxd
 
 // REST
 let api = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/',
+  baseURL: 'https://api.yelp.com/v3/',
   headers: {
     Authorization: `Bearer ${API_KEY}`,
     "Content-type": "application/json",
