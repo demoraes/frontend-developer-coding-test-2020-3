f6ac821063b6e85cc92cb64c2e9a4a23c36a0ebc
Gabriel Moraes <cbgabriel2016@gmail.com>
2020-12-08 23:51:41 -0400

    Update index.js

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -30,7 +30,7 @@ export default function Main() {
       },
     });
     
-    setRestaurants(response.data.search);
+    setRestaurants(response.data.search.businesses);
     setIsLaoading(false);
   }
     
