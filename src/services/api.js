1bc1738ca66d6ab9f4ea496c2cb9270f9f64cb13
Gabriel Moraes <cbgabriel2016@gmail.com>
2020-12-08 22:21:38 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = "ntzIk5iasX7PLW5ybOi-2aaWyRTA6PTwWi4Da9VWUJ-3CL9w8r5aV1hdg94JGxd7OjuckoCLKtUyzuotRbAMnJDptMqCQMq_xqFRLa1nM38FUP-fLNKEO2bC9OPLX3Yx";
 
 // REST
-let api = axios.create({
+const api = axios.create({
   baseURL: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/',
   headers: {
     Authorization: `Bearer ${API_KEY}`,
